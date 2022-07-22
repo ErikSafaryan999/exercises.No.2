4f8315adeb18320a55d7f39cfc0434fa67b5275b
erosa <erosa@LAPTOP-GEUC14DB>
2022-07-22 20:11:32 +0400

    Add project files.

---
+++ exercises.No.2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercises.No._2
+{
+    class Class1
+    {
+            // Поля класса
+            public string Name, Family, Adress;
+            public byte Age;
+
+            // Метод, выводящий в консоль контактную информацию
+            public void writeInConsoleInfo()
+            {
+                Console.WriteLine($"Name: {Name}\nLastName: {Family}\nAdress:{Adress}\nAge:{Age}");
+            }
+    }
+}
---
+++ exercises.No.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercises.No._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 myInfo = new Class1();
+
+            myInfo.Name = "Erik";
+            myInfo.Family = "Safaryan";
+            myInfo.Adress = "Yerevan";
+            myInfo.Age = 20;
+
+            myInfo.writeInConsoleInfo();
+        }
+    }
+}
